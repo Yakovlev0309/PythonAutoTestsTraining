b05859ef4be740745a6dec28ec1e3eb595f53bb4
Nikita_Yakovlev <nyakovlev@npo-rit.ru>
2024-01-25 15:06:13 +0300

    Тест добавления нового контакта

---
+++ contact.py
@@ -0,0 +1,19 @@
+
+
+class BirthDate:
+    def __init__(self, bday, bmonth, byear):
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+
+
+class Contact:
+    def __init__(self, firstname, lastname, nickname, company, address, birthDate, notes):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.birthDate = birthDate
+        self.notes = notes
+
