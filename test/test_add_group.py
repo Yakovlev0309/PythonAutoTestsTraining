1737a4da2cd349c1a6d91e1933b5941ded6ba475
Yakovlev0309 <yakovlev.universe@gmail.com>
2024-01-25 16:15:58 +0300

    Созданы пакеты для разных зон ответственности

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from application import Application
-from group import Group
+from fixture.application import Application
+from model.group import Group
 
 
 @pytest.fixture
@@ -15,15 +15,17 @@ def test_add_group(app):
     app.openHomePage()
     app.login(username="admin", password="secret")
     app.openGroupsPage()
-    app.createGroup(Group(name="test group", header="test header", footer="test footer"))
-    app.returnToGroupsPage()        
+    app.createGroup(
+        Group(name="test group", header="test header", footer="test footer")
+    )
+    app.returnToGroupsPage()
     app.logout()
-    
-        
+
+
 def test_add_empty_group(app):
     app.openHomePage()
     app.login(username="admin", password="secret")
     app.openGroupsPage()
     app.createGroup(Group(name="", header="", footer=""))
-    app.returnToGroupsPage()        
+    app.returnToGroupsPage()
     app.logout()
