195bd21bc17db1deba389b42b5b567c5489394ba
Yakovlev0309 <yakovlev.universe@gmail.com>
2024-02-01 10:12:39 +0300

    Выбор случайной группы в тестах удаления и модификации групп

--- fixture/group.py
+++ fixture/group.py
@@ -39,27 +39,36 @@ class GroupHelper:
         self.group_cache = None
 
     def select_first_group(self):
-        self.app.wd.find_element("name", "selected[]").click()
+        self.select_group_by_index(0)
 
-    def delete_first_group(self):
+    def select_group_by_index(self, index):
+        self.app.wd.find_elements("name", "selected[]")[index].click()
+
+    def test_delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element("name", "delete").click()
         # return to groups page
         self.return_to_groups_page()
         self.group_cache = None
 
-    def modify_first_group(self, new_group_data):
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         wd.find_element("name", "edit").click()
         self.fill_group_form(new_group_data)
         wd.find_element("name", "update").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+    def modify_first_group(self, new_group_data):   
+        self.modify_group_by_index(0, new_group_data)
 
     def count(self):
         self.open_groups_page()
