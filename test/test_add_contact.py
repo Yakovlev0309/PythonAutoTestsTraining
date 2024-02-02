22f420985d7a13eab3666a882cd7da273cf3a1e1
Yakovlev0309 <yakovlev.universe@gmail.com>
2024-02-02 15:19:31 +0300

    Параметризация и рандомизация теста test_contact_group

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,25 @@
 from model.contact import Contact, BirthDate
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
-    bdate = BirthDate(bday="28", bmonth="June", byear="1971")
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="Elon", lastname="Musk", nickname="elona51",
+                    company="Tesla, SpaceX", address="California, Silicon Valley",
+                    homephone="48-22-56", mobilephone="8-800-555-35-35", workphone="666-666", secondaryphone="1-234-567-89-10",
+                    birth_date=BirthDate(bday="28", bmonth="June", byear="1971"), notes="Smart person (not an alian)")] + \
+                    [Contact(firstname=random_string("", 10), lastname=random_string("", 10))
+                     for i in range(4)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Elon", lastname="Musk", nickname="elona51", \
-                      company="Tesla, SpaceX", address="California, Silicon Valley", 
-                      homephone="48-22-56", mobilephone="8-800-555-35-35", workphone="666-666", secondaryphone="1-234-567-89-10", 
-                      birth_date=bdate, notes="Smart person (not an alian)")
     app.contact.add(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
