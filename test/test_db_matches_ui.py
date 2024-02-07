f170be1573e0e13a821fc50c22dac054e436ccdd
Yakovlev0309 <yakovlev.universe@gmail.com>
2024-02-07 09:39:00 +0300

    Тест сравнения всех контактов с информацией из БД

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,4 +1,5 @@
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(app, db): # app - доступ к пользовательскому интерфейсу, db - доступ к базе данных
@@ -7,3 +8,12 @@ def test_group_list(app, db): # app - доступ к пользовательс
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname, lastname=contact.lastname, address=contact.address, 
+                       all_emails_from_home_page=contact.all_emails_from_home_page, all_phones_from_home_page=contact.all_phones_from_home_page)
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
