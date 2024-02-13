e98fd63221636b89cafb9831a3b6ca1b35518586
Yakovlev0309 <yakovlev.universe@gmail.com>
2024-02-13 09:55:42 +0300

    -Тесты для создания (падают) и удаления групп с помощью BDD

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,12 @@
+from pytest_bdd import scenario
+from bdd.group_steps import *
+
+
+@scenario(feature_name="groups.feature", scenario_name="Add new group")
+def test_add_group():
+    pass
+
+
+@scenario(feature_name="groups.feature", scenario_name="Delete a group")
+def test_delete_group():
+    pass
